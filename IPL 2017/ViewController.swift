23793042f9329c8b8fdf7fedb77e4c24c7246ec7
umairaslam1996 <umairasalam1996@gmail.com>
2017-02-22 22:13:26 +0500

    Initial Commit

---
+++ IPL 2017/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IPL 2017
+//
+//  Created by Umair Aslam on 22/02/2017.
+//  Copyright © 2017 mc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ IPL 2017Tests/IPL_2017Tests.swift
@@ -0,0 +1,36 @@
+//
+//  IPL_2017Tests.swift
+//  IPL 2017Tests
+//
+//  Created by Umair Aslam on 22/02/2017.
+//  Copyright © 2017 mc. All rights reserved.
+//
+
+import XCTest
+@testable import IPL_2017
+
+class IPL_2017Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
